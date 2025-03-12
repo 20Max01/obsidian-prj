f243d23b3723072b7d46dfe4847f24824257502d
Max P. <mail@mpassarello.de>
2025-03-12 16:11:36 +0100

    Add StaticMetadataDecorator and tests - Introduced `StaticMetadataDecorator.ts` with:   - `IStaticMetadataField_` interface   - `ModifierDelegate` type   - `staticMetadata` decorator function   - `getStaticMetadata` function - Added unit tests in `StaticMetadataDecorator.test.ts` to verify:   - Error handling when target is not a class property   - Creation of metadata field   - Passing of parent metadata to `modifierDelegate`

---
+++ src/classes/decorators/StaticMetadataDecorator.ts
@@ -0,0 +1,79 @@
+interface IStaticMetadataField_<T> {
+    [key: PropertyKey]: T;
+}
+
+function isIStaticMetadataField_<T>(
+    obj: unknown,
+): obj is IStaticMetadataField_<T> {
+    return obj != null && typeof obj === 'function';
+}
+
+/**
+ * A delegate to modify the shadow field.
+ * @param parentShadowField The shadow field of the parent class.
+ * @param shadowField The shadow field of the current class.
+ * @returns The modified shadow field.
+ */
+export type ModifierDelegate<T> = (
+    parentShadowField: T | undefined,
+    shadowField: T | undefined,
+) => T;
+
+/**
+ * A decorator function to add a static metadata field to a class.
+ * @param shadowFieldKey The key of the shadow field.
+ * @param modifierDelegate The delegate to modify the shadow field.
+ * @returns A decorator function.
+ */
+export function staticMetadata<T>(
+    shadowFieldKey: PropertyKey,
+    modifierDelegate: ModifierDelegate<T>,
+) {
+    return function (target: unknown, propertyKey: string | symbol): void {
+        if (!target || !isIStaticMetadataField_<T>(target.constructor)) {
+            throw new Error(
+                'The static metadata decorator can only be used on class properties.',
+            );
+        }
+        const parent = Object.getPrototypeOf(target.constructor);
+
+        const parentShadowField =
+            parent && isIStaticMetadataField_<T>(parent)
+                ? Object.getOwnPropertyDescriptor(parent, shadowFieldKey)?.value
+                : undefined;
+
+        const targetShadowField = Object.getOwnPropertyDescriptor(
+            target.constructor,
+            shadowFieldKey,
+        );
+
+        target.constructor[shadowFieldKey] = modifierDelegate(
+            parentShadowField,
+            targetShadowField?.value,
+        );
+    };
+}
+
+/**
+ * Gets the static metadata field from the target.
+ * @param target The target to get the metadata from.
+ * @param shadowFieldKey The key of the shadow field.
+ * @returns The metadata value.
+ */
+export function getStaticMetadata<T>(
+    target: unknown,
+    shadowFieldKey: PropertyKey,
+): T | undefined {
+    if (!target || !isIStaticMetadataField_<T>(target.constructor)) {
+        throw new Error(
+            'The metadata decorator can only be used on class properties.',
+        );
+    }
+
+    const targetShadowField = Object.getOwnPropertyDescriptor(
+        target.constructor,
+        shadowFieldKey,
+    );
+
+    return targetShadowField?.value;
+}
---
+++ src/classes/decorators/__tests__/StaticMetadataDecorator.test.ts
@@ -0,0 +1,68 @@
+import { getStaticMetadata, staticMetadata } from '../StaticMetadataDecorator';
+
+describe('Static Metadata Decorator Tests', () => {
+    const MetadataSymbol: unique symbol = Symbol('Metadata');
+
+    it('should throw an error if the target is not a class property', () => {
+        expect(() => {
+            staticMetadata(MetadataSymbol, () => 'test')(null, 'test');
+        }).toThrow(
+            'The static metadata decorator can only be used on class properties.',
+        );
+    });
+
+    it('should create metadata field', () => {
+        class TestClass {
+            @staticMetadata(MetadataSymbol, () => 'test')
+            public testField: string;
+        }
+
+        const testInstance = new TestClass();
+        expect(getStaticMetadata(testInstance, MetadataSymbol)).toBe('test');
+    });
+
+    it('should pass the parent metadata to the modifierDelegate', () => {
+        class ParentClass {
+            @staticMetadata(MetadataSymbol, () => 'parent')
+            public testField: string;
+        }
+
+        class TestClass extends ParentClass {
+            @staticMetadata(MetadataSymbol, (parent, current) => {
+                return `${parent} ${current}`;
+            })
+            public testField: string;
+        }
+
+        const testInstance = new TestClass();
+
+        expect(getStaticMetadata(testInstance, MetadataSymbol)).toBe(
+            'parent undefined',
+        );
+    });
+
+    it('should pass the parent metadata to the modifierDelegate for multiple fields', () => {
+        class ParentClass {
+            @staticMetadata<string>(MetadataSymbol, () => 'parent')
+            public testField: string;
+        }
+
+        class TestClass extends ParentClass {
+            @staticMetadata<string>(MetadataSymbol, (parent, current) => {
+                return `${parent} ${current}`;
+            })
+            public testField: string;
+
+            @staticMetadata<string>(MetadataSymbol, (parent, current) => {
+                return `${parent} ${current}`;
+            })
+            public testField2: string;
+        }
+
+        const testInstance = new TestClass();
+
+        expect(getStaticMetadata(testInstance, MetadataSymbol)).toBe(
+            'parent parent undefined',
+        );
+    });
+});
