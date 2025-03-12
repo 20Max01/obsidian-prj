4770bf5f08e384b4a43dd0e099c3ec24e334750f
Max P. <mail@mpassarello.de>
2025-03-12 18:42:43 +0100

    Refactor `toStringField` decorator logic
    - Removed `IToStringField_` interface and its type guard function.
    - Introduced `toStringFieldConfigurator` using `ModifierDelegate`.
    - Utilized `staticMetadata` to manage `ToStringFieldSymbol`.
    - Simplified logic for adding property keys to `ToStringFieldSymbol`.

--- src/classes/decorators/ToStringFieldDecorator.ts
+++ src/classes/decorators/ToStringFieldDecorator.ts
@@ -1,25 +1,10 @@
 import PrjBaseData from 'src/models/Data/PrjBaseData';
+import { ModifierDelegate, staticMetadata } from './StaticMetadataDecorator';
 
 /**
  * A unique symbol used to mark properties that should be included in the {@link PrjBaseData.toString} method.
  */
 export const ToStringFieldSymbol: unique symbol = Symbol('ToStringField');
-
-/**
- * Represents the field configuration properties.
- */
-interface IToStringField_ {
-    [ToStringFieldSymbol]?: string[];
-}
-
-/**
- * Type guard to check if an object is an instance of {@link IToStringField_}.
- * @param obj The object to check.
- * @returns Ever `True` because the {@link ToStringFieldSymbol} property is optional.
- */
-function isIToStringField_(obj: unknown): obj is IToStringField_ {
-    return true;
-}
 
 /**
  * A decorator function to mark class properties for inclusion in the toString output.
@@ -31,23 +16,20 @@ export function toStringField(
     target: unknown,
     propertyKey: string | symbol,
 ): void {
-    // Check if the target is an object guard the optional `ToStringFieldSymbol` property.
-    if (
-        !target ||
-        typeof target !== 'object' ||
-        !(target instanceof Object) ||
-        !isIToStringField_(target.constructor)
-    ) {
-        throw new Error(
-            'The toStringField decorator can only be used on class properties.',
-        );
-    }
+    const toStringFieldConfigurator: ModifierDelegate<string[]> = (
+        parentToStringFields,
+        toStringFields,
+        propertyKey,
+    ) => {
+        if (toStringFields === undefined) toStringFields = [];
 
-    // If the class does not have a `ToStringFieldSymbol` property, create it.
-    if (!target.constructor[ToStringFieldSymbol]) {
-        target.constructor[ToStringFieldSymbol] = [];
-    }
+        toStringFields.push(propertyKey.toString());
 
-    // Push the property key to the `ToStringFieldSymbol` property.
-    target.constructor[ToStringFieldSymbol].push(propertyKey.toString());
+        return toStringFields;
+    };
+
+    staticMetadata(ToStringFieldSymbol, toStringFieldConfigurator)(
+        target,
+        propertyKey,
+    );
 }
